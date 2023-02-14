ff57b285083b21e4572feffca1432ad08e3f49c2
Ezequiel Farina <ezequielfarina@ezequielfarina.local>
2023-02-14 18:14:08 -0300

    Corregido formateo

--- src/productManager.js
+++ src/productManager.js
@@ -1,98 +1,98 @@
 var fs = require('fs');
 
- class ProductManager {
-    constructor() {
-      this.products = [];
-      this.path = "./";
-    }
-  
-    addProduct(product) {
-      if (
-        product.title != "" &&
-        product.description != "" &&
-        product.price != "" &&
-        product.thumbnail != "" &&
-        product.stock != "" &&
-        product.code != ""
-      ) {
-        if (this.products.length > 0) {
-          for (let productB of this.products) {
-            if (product.code === productB.code) {
-              return null;
-            }
+class ProductManager {
+  constructor() {
+    this.products = [];
+    this.path = "./";
+  }
+
+  addProduct(product) {
+    if (
+      product.title != "" &&
+      product.description != "" &&
+      product.price != "" &&
+      product.thumbnail != "" &&
+      product.stock != "" &&
+      product.code != ""
+    ) {
+      if (this.products.length > 0) {
+        for (let productB of this.products) {
+          if (product.code === productB.code) {
+            return null;
           }
         }
-        let id = performance.now();
-        product.id = id;
-        this.products.push(product);
-        return id;
       }
-      return null;
+      let id = performance.now();
+      product.id = id;
+      this.products.push(product);
+      return id;
     }
-  
-    getProducts() {
-      return this.products;
+    return null;
+  }
+
+  getProducts() {
+    return this.products;
+  }
+
+  getProductById(id) {
+    const found = this.products.find((p) => (p.id = id));
+    if (found === null) {
+      console.log("not found");
     }
-  
-    getProductById(id) {
-      const found = this.products.find((p) => (p.id = id));
-      if (found === null){
-          console.log("not found");
+    return found;
+  }
+
+  /*Debe tener un método updateProduct, el cual debe recibir el id del producto a actualizar, 
+  así también como el campo a actualizar (puede ser el objeto completo, como en una DB), 
+  y debe actualizar el producto que tenga ese id en el archivo. NO DEBE BORRARSE SU ID 
+  Debe tener un método deleteProduct, el cual debe recibir un id y 
+  debe eliminar el producto que tenga ese id en el archivo.
+ */
+  updateProduct(product) {
+    console.log(JSON.stringify(product));
+
+    console.log(JSON.stringify(products));
+    for (let productB of this.products) {
+      if (product.id === productB.id) {
+
+        productB.description = product.description;
+        productB.price = product.price;
+        productB.title = product.title;
+        productB.thumbnail = product.thumbnail;
+        productB.stock = product.stock;
+        productB.code = product.code;
+        fs.writeFileSync(this.path + "Productos.txt", JSON.stringify(products));
+
+        return;
       }
-      return found;
+
     }
 
-    /*Debe tener un método updateProduct, el cual debe recibir el id del producto a actualizar, 
-    así también como el campo a actualizar (puede ser el objeto completo, como en una DB), 
-    y debe actualizar el producto que tenga ese id en el archivo. NO DEBE BORRARSE SU ID 
-    Debe tener un método deleteProduct, el cual debe recibir un id y 
-    debe eliminar el producto que tenga ese id en el archivo.
-   */
-    updateProduct(product) {
-        console.log(JSON.stringify(product));
-        
-        console.log(JSON.stringify(products));
-        for (let productB of this.products) {
-            if (product.id === productB.id) {
-
-                productB.description = product.description;
-                productB.price = product.price;
-                productB.title = product.title;
-                productB.thumbnail = product.thumbnail;
-                productB.stock  = product.stock;
-                productB.code = product.code;
-                fs.writeFileSync(this.path + "Productos.txt", JSON.stringify(products));
-
-                return;
-            }
-
-        }    
-
-        console.log("producto no encontrados");
-    }
-
-    deleteProduct(id) {
-
-        this.products = this.products.filter((p) => (p.id != id));
-        console.log(JSON.stringify(this.products));
-        fs.writeFileSync(this.path + "Productos.txt", JSON.stringify(this.products));
-    }
-    
+    console.log("producto no encontrados");
   }
-   
-  
-  class Product {
-    constructor(title, description, price, thumbnail, code, stock, id ) {
-      this.title = title;
-      this.description = description;
-      this.price = price;
-      this.thumbnail = thumbnail;
-      this.stock = stock;
-      this.code = code;
-      this.id = id;
-    }
-  }
-  
-  const productM = new ProductManager();
 
-  
+  deleteProduct(id) {
+
+    this.products = this.products.filter((p) => (p.id != id));
+    console.log(JSON.stringify(this.products));
+    fs.writeFileSync(this.path + "Productos.txt", JSON.stringify(this.products));
+  }
+
+}
+
+
+class Product {
+  constructor(title, description, price, thumbnail, code, stock, id) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.thumbnail = thumbnail;
+    this.stock = stock;
+    this.code = code;
+    this.id = id;
+  }
+}
+
+const productM = new ProductManager();
+
+
--- src/server.js
+++ src/server.js
@@ -7,33 +7,33 @@ app.use(express.static("public"));
 // Se mandará a llamar desde el navegador a la url http://localhost:8080/products?limit=5 , eso debe devolver sólo los primeros 5 de los 10 productos.
 //  Se mandará a llamar desde el navegador a la url http://localhost:8080/products sin query, eso debe devolver todos los 10 productos.
 app.get('/products', (req, res) => {
-    let listado = productM.getProducts ();
+    let listado = productM.getProducts();
     let limit = req.query.limit;
     if (limit === undefined) {
         res.send(JSON.stringify(listado));
     } else {
         res.send(JSON.stringify(listado.slice(0, limit)));
     }
-    
-    
+
+
 });
 
 // Se mandará a llamar desde el navegador a la url http://localhost:8080/products/2, eso debe devolver sólo el producto con id=2.
 // Se mandará a llamar desde el navegador a la url http://localhost:8080/products/34123123, al no existir el id del producto, debe devolver un objeto con un error indicando que el producto no existe.
 app.get('/products/:id', (req, res) => {
     let product = productM.getProductById(req.params.id);
-    if (product === null){
-        res.send ("producto no encontrado ");
-    } else{
+    if (product === null) {
+        res.send("producto no encontrado ");
+    } else {
         res.send(JSON.stringify(product));
     }
-    
+
 });
 
-const PORT=8080;
- const server=app.listen(PORT,()=>{
+const PORT = 8080;
+const server = app.listen(PORT, () => {
     console.log("servidor ejecutandose en el puerto: ", PORT);
- });
+});
 
 
 
